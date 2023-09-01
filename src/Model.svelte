eabef89cb775a6ac9745fef2c1dfff4281e6edb7
rinto11804 <rinto11804@gmail.com>
2023-09-01 17:22:17 +0530

    Fixed null text  and text persisting issue in model

--- src/Model.svelte
+++ src/Model.svelte
@@ -1,11 +1,11 @@
 <script>
-    import { addCard, openModal , selectedColumn} from "./store";
+    import { addCard, openModal, selectedColumn } from "./store";
 
     let dialog;
 
-    let title;
-    let content;
-    let tag;
+    let title = '';
+    let content = '';
+    let tag = '';
 
     function closeModel() {
         dialog.close();
@@ -21,30 +21,53 @@
     bind:this={dialog}
     on:close={() => ($openModal = false)}
     on:click|self={() => dialog.close()}
-
 >
-
     <h2>Add a new task</h2>
     <form method="dialog">
         <label for="title">
             <span>Title</span>
-            <input type="text" bind:value={title}/>
+            <input type="text" bind:value={title} required />
         </label>
 
         <label for="content">
             <span>Content</span>
-            <textarea name="content" id="content" cols="30" rows="10" bind:value={content}/>
+            <textarea
+                name="content"
+                id="content"
+                cols="30"
+                rows="5"
+                bind:value={content}
+                required
+            />
         </label>
 
         <label for="tag">
             <span>Tag</span>
-            <input type="text" bind:value={tag}/>
+            <input type="text" bind:value={tag} required />
         </label>
 
         <div class="btn">
             <button on:click={closeModel} class="normal-btn">Close</button>
             <!-- svelte-ignore a11y-autofocus -->
-            <button type="submit" on:click={() => addCard({title,content,tag,column_id:$selectedColumn})} autofocus>Submit</button>
+            <button
+                type="submit"
+                on:click={() => {
+                    if (title !== "" && content !== "" && tag !== "") {
+                        addCard({
+                            title,
+                            content,
+                            tag,
+                            column_id: $selectedColumn,
+                        });
+                        title = "";
+                        content = "";
+                        tag = "";
+
+                        closeModel();
+                    }
+                }}
+                autofocus>Submit</button
+            >
         </div>
     </form>
 </dialog>
@@ -60,7 +83,7 @@
         border: 1px solid var(--border);
         border-radius: var(--radius);
     }
-    h2{
+    h2 {
         margin-bottom: 1rem;
     }
 
@@ -86,11 +109,13 @@
     }
 
     :is(input, textarea) {
+        font-family: inherit;
         background-color: transparent;
         border-color: var(--border);
         padding: 0.25rem;
         border-radius: var(--radius);
         font-size: 1rem;
+        font-weight: 300;
         padding: 0.45rem;
         margin-top: 0.25rem;
     }
